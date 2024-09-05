468d48960e7cce30cc530a7541a8af7aae2de0af
GrkDev <eligombis.yt3@gmail.com>
2024-09-05 08:09:03 -0500

    Initialize

---
+++ src/main.py
@@ -0,0 +1,19 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# 	Module:       main.py                                                      #
+# 	Author:       eligo                                                        #
+# 	Created:      9/5/2024, 8:04:51 AM                                         #
+# 	Description:  V5 project                                                   #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+# Library imports
+from vex import *
+
+# Brain should be defined by default
+brain=Brain()
+
+brain.screen.print("Hello V5")
+
+
+        
