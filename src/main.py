5c47d8ff772c94afa8ca8aeceb6e5d0955185c42
GrkDev <eligombis.yt3@gmail.com>
2024-09-13 14:38:17 -0500

    DriveTrain Draft

--- src/main.py
+++ src/main.py
@@ -13,10 +13,36 @@ from vex import *
 # Brain should be defined by default
 brain=Brain()
 
-motorTest = Motor(Ports.PORT1)
+controller = Controller()
+
+leftOne = Motor(Ports.PORT1)
+leftTwo = Motor(Ports.PORT2)
+rightOne = Motor(Ports.PORT3)
+rightTwo = Motor(Ports.PORT4)
+
+speed = 100
 
 def driveTask():
-    motorTest.set_velocity(75, RPM)
-    motorTest.spin(FORWARD)
+    while True:
+        driveStraight = controller.axis3.position()
+        driveTurn = controller.axis1.position()
+        if driveTurn == 0:
+            leftOne.spin(FORWARD, driveStraight, PERCENT)
+            leftTwo.spin(FORWARD, driveStraight, PERCENT)
+            rightOne.spin(FORWARD, driveStraight, PERCENT)
+            rightTwo.spin(FORWARD, driveStraight, PERCENT)
+        elif driveTurn < 0:
+            driveTurn = abs(driveTurn)
+            leftOne.spin(FORWARD, driveTurn, PERCENT)
+            leftTwo.spin(FORWARD, driveTurn, PERCENT)
+            rightOne.spin(FORWARD, driveTurn * 0.6, PERCENT)
+            rightTwo.spin(FORWARD, driveTurn * 0.6, PERCENT)
+        elif driveTurn > 0:
+            leftOne.spin(FORWARD, driveTurn * 0.6, PERCENT)
+            leftTwo.spin(FORWARD, driveTurn * 0.6, PERCENT)
+            rightOne.spin(FORWARD, driveTurn, PERCENT)
+            rightTwo.spin(FORWARD, driveTurn, PERCENT)
+            
+
 
 drive = Thread(driveTask)
