40050dd6ce27f0fccde68699945fccd63c21bf09
GrkDev <eligombis.yt3@gmail.com>
2024-10-07 16:46:12 -0500

    Added ability to turn and move forward/backward, added code for conveyor, changed controls

--- src/main.py
+++ src/main.py
@@ -14,30 +14,74 @@ from vex import *
 brain=Brain()
 
 controller = Controller()
+controllerEnabled = True
 
-rightFront = Motor(Ports.PORT1)
-rightBack = Motor(Ports.PORT2)
-leftFront = Motor(Ports.PORT3)
-leftBack = Motor(Ports.PORT4)
+rightFront = Motor(Ports.PORT3)
+rightBack = Motor(Ports.PORT4)
+leftFront = Motor(Ports.PORT1) 
+leftBack = Motor(Ports.PORT2)
+
+intakeMotor = Motor(Ports.PORT10)
+conveyorMotor = Motor(Ports.PORT20)
 
 speed = 100
 
 def driveTask():
-    while True:
-        driveStraight = controller.axis1.position()
-        driveTurn = controller.axis3.position()
+    if controllerEnabled:
+        while True:
+            driveStraight = controller.axis2.position()
+            driveTurn = controller.axis4.position()
 
-        if driveTurn == 0:
-            rightFront.spin(FORWARD, driveStraight, PERCENT)
-            rightBack.spin(REVERSE, driveStraight, PERCENT)
-            leftBack.spin(FORWARD, driveStraight, PERCENT)
-            leftFront.spin(REVERSE, driveStraight, PERCENT)
-        else:
-            rightFront.spin(FORWARD, driveTurn, PERCENT)
-            rightBack.spin(REVERSE, driveTurn, PERCENT)
-            leftFront.spin(REVERSE, driveTurn, PERCENT)
-            leftBack.spin(FORWARD, driveTurn, PERCENT)
+            rightDrive = driveStraight - driveTurn
+            leftDrive = driveStraight + driveTurn
+            x = 1
+
+            if rightDrive > 100 or rightDrive < -100:
+                x = rightDrive / 100
+
+                leftDrive /= x
+
+            if leftDrive > 100 or leftDrive < -100:
+                x = leftDrive / 100
+
+                rightDrive /= x
+
+            rightFront.spin(REVERSE, rightDrive, PERCENT)
+            rightBack.spin(FORWARD, rightDrive, PERCENT)
+            leftBack.spin(REVERSE, leftDrive, PERCENT)
+            leftFront.spin(FORWARD, leftDrive, PERCENT)
+
+def intakeForward():
+    intakeMotor.spin(REVERSE, 100, PERCENT)
+
+def intakeBackward():
+    intakeMotor.spin(FORWARD, 100, PERCENT)
             
+def intakeStop():
+    intakeMotor.stop()
 
+def conveyorStart():
+    conveyorMotor.spin(FORWARD, 100, PERCENT)
+
+def conveyorStop():
+    conveyorMotor.stop()
+
+def compDrive():
+    controllerEnabled = True
+
+def autonomous():
+    controllerEnabled = False
 
 drive = Thread(driveTask)
+
+competition = Competition(driveTask, autonomous)
+
+
+controller.buttonL2.pressed(intakeForward)
+controller.buttonL2.released(intakeStop)
+
+controller.buttonL1.pressed(intakeBackward)
+controller.buttonL1.released(intakeStop)
+
+controller.buttonR2.pressed(conveyorStart)
+controller.buttonR2.released(conveyorStop)
