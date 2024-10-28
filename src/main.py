9d7f1c424b84f14657cedf22ac9929a7fca74186
GrkDev <eligombis.yt3@gmail.com>
2024-10-28 15:47:35 -0500

    Figured out how to test autonomous and adjusted some values

--- src/main.py
+++ src/main.py
@@ -16,40 +16,48 @@ brain=Brain()
 controller = Controller()
 controllerEnabled = True
 
-rightFront = Motor(Ports.PORT3)
+rightFront = Motor(Ports.PORT3, True)
 rightBack = Motor(Ports.PORT4)
-leftFront = Motor(Ports.PORT1) 
+leftFront = Motor(Ports.PORT1, True) 
 leftBack = Motor(Ports.PORT2)
 
-intakeMotor = Motor(Ports.PORT10)
+left = MotorGroup(leftFront, leftBack)
+right = MotorGroup(rightFront, rightBack)
+
+piston = Pneumatics(brain.three_wire_port.h)
+
+driveTrain = DriveTrain(left, right, 70, 422, 319, MM, 0.5)
+
+
+intakeMotor = Motor(Ports.PORT11)
 conveyorMotor = Motor(Ports.PORT20)
 
-speed = 100
+def autonTesting():
+    driveTrain.turn_for(RIGHT, 90, DEGREES)
+
 
 def driveTask():
-    if controllerEnabled:
-        while True:
-            driveStraight = controller.axis2.position()
-            driveTurn = controller.axis4.position()
+    while True:
+        driveStraight = -1 * controller.axis4.position()
+        driveTurn = controller.axis2.position()
 
-            rightDrive = driveStraight - driveTurn
-            leftDrive = driveStraight + driveTurn
-            x = 1
+        rightDrive = driveStraight - driveTurn
+        leftDrive = driveStraight + driveTurn
+        x = 1
 
-            if rightDrive > 100 or rightDrive < -100:
-                x = rightDrive / 100
+        if rightDrive > 100 or rightDrive < -100:
+            x = rightDrive / 100
 
-                leftDrive /= x
+            leftDrive /= x
 
-            if leftDrive > 100 or leftDrive < -100:
-                x = leftDrive / 100
+        if leftDrive > 100 or leftDrive < -100:
+            x = leftDrive / 100
 
-                rightDrive /= x
+            rightDrive /= x
 
-            rightFront.spin(REVERSE, rightDrive, PERCENT)
-            rightBack.spin(FORWARD, rightDrive, PERCENT)
-            leftBack.spin(REVERSE, leftDrive, PERCENT)
-            leftFront.spin(FORWARD, leftDrive, PERCENT)
+        right.spin(FORWARD, rightDrive, PERCENT)
+        left.spin(FORWARD, leftDrive, PERCENT)
+        
 
 def intakeForward():
     intakeMotor.spin(REVERSE, 100, PERCENT)
@@ -61,20 +69,37 @@ def intakeStop():
     intakeMotor.stop()
 
 def conveyorStart():
-    conveyorMotor.spin(FORWARD, 100, PERCENT)
+    conveyorMotor.spin(FORWARD, 60, PERCENT)
+
+def conveyorBackwards():
+    conveyorMotor.spin(REVERSE, 60, PERCENT)
 
 def conveyorStop():
     conveyorMotor.stop()
+
+def pistonOpen():
+    piston.open()
+
+def pistonClose():
+    piston.close()
 
 def compDrive():
     controllerEnabled = True
 
 def autonomous():
-    controllerEnabled = False
+    brain.screen.print("Auton")
 
-drive = Thread(driveTask)
+def stopMotors():
+    rightFront.stop()
+    rightBack.stop()
+    leftFront.stop()
+    leftBack.stop()
 
-competition = Competition(driveTask, autonomous)
+
+
+# drive = Thread(driveTask)
+
+# competition = Competition(driveTask, autonTesting)
 
 
 controller.buttonL2.pressed(intakeForward)
@@ -85,3 +110,12 @@ controller.buttonL1.released(intakeStop)
 
 controller.buttonR2.pressed(conveyorStart)
 controller.buttonR2.released(conveyorStop)
+
+controller.buttonR1.pressed(conveyorBackwards)
+controller.buttonR1.released(conveyorStop)
+
+controller.buttonA.pressed(autonTesting)
+controller.buttonA.released(stopMotors)
+
+controller.buttonUp.pressed(pistonOpen)
+controller.buttonDown.pressed(pistonClose)
